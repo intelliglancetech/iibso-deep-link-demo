e25a29b554156ac1b627eab560a42ae050f9a0a1
Tech Glance <96744507+techglance360@users.noreply.github.com>
2022-07-30 00:37:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
- window.onload = () => {
-      var ua = detect.parse(navigator.userAgent);
-      document.write(ua.os.family);
-      
-      if(ua.os.family == "Android"||ua.os.family == "IOS"){
-        document.addEventListener("DOMContentLoaded", function (event) {
-          var links = document.querySelectorAll("a");
-          var baseUri = "exp://wg-qka.notbrent.app.exp.direct";
+const GOOGLE_PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=com.techglance.easypdf";
+const IOS_APP_STORE_LINK = "https://apps.apple.com/us/app/foodpanda-food-delivery/id758103884";
+
+window.onload = (function handleUserRedirection (window={}, document={}) {
+  let ua = detect.parse(navigator.userAgent);
   
-          // Take the uri from the params
-          var qs = decodeURIComponent(document.location.search);
-          if (qs) {
-            baseUri = qs.split("?linkingUri=")[1];
-          }
+  document.write(ua.os.family);
   
-          // Update the link urls
-          // for (var i = 0; i < links.length; ++i) {
-          //   links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
-          //   links[i].textContent = links[i].href
-          // }
-  
-          var redirectInterval = setInterval(function () {
-            var countdown = document.querySelector(".countdown");
-            var t = parseInt(countdown.innerText, 10);
-            t -= 1;
-  
-            countdown.innerText = t;
-  
-            if (t === 0) {
-              clearInterval(redirectInterval);
-              window.location.href =
-                baseUri +
-                "message=" +
-                encodeURIComponent("Redirected automatically by timer");
-            }
-          }, 1000);
-        });
+  if (ua.os.family == "Android" || ua.os.family == "IOS") {
+    document.addEventListener("DOMContentLoaded", function (event) {
+      var links = document.querySelectorAll("a");
+      var baseUri = "exp://wg-qka.notbrent.app.exp.direct";
+
+      // Take the uri from the params
+      var qs = decodeURIComponent(document.location.search);
+      if (qs) {
+        baseUri = qs.split("?linkingUri=")[1];
       }
-      else{
-        window.location.href = "https://play.google.com/store/apps/details?id=com.techglance.easypdf";
-      }
-    };
+
+      // Update the link urls
+      // for (var i = 0; i < links.length; ++i) {
+      //   links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
+      //   links[i].textContent = links[i].href
+      // }
+
+      var redirectInterval = setInterval(function () {
+        var countdown = document.querySelector(".countdown");
+        var t = parseInt(countdown.innerText, 10);
+        t -= 1;
+
+        countdown.innerText = t;
+
+        if (t === 0) {
+          clearInterval(redirectInterval);
+          window.location.href =
+            baseUri +
+            "message=" +
+            encodeURIComponent("Redirected automatically by timer");
+        }
+      }, 1000);
+    });
+  } else {
+    window.location.href = ua.os.family == "Android" ? GOOGLE_PLAY_STORE_LINK : ua.os.family == "IOS" ? IOS_APP_STORE_LINK : GOOGLE_PLAY_STORE_LINK
+  }
+})(window, document);
