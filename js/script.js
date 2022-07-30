f5ac7c0ca27001433d6f9e4f656747c949e047d7
MuhammadUmar00 <darazkhan250@gmail.com>
2022-07-30 05:22:12 +0500

    refactor: added the missing image

--- js/script.js
+++ js/script.js
@@ -8,35 +8,35 @@ window.onload = (function handleUserRedirection(window = {}, document = {}) {
 
   if (ua.os.family == "Android" || ua.os.family == "IOS") {
     document.addEventListener("DOMContentLoaded", function (event) {
-      let links = document.querySelectorAll("a");
-      let baseUri = "exp://wg-qka.notbrent.app.exp.direct/productDetail";
-      
+      var links = document.querySelectorAll("a");
+      // var baseUri = "exp://wg-qka.notbrent.app.exp.direct";
+      var baseUri;
+
       // Take the uri from the params
-      let qs = decodeURIComponent(document.location.search);
-      let purpose = qs.split("?purpose")[1];
-      let productId = qs.split("?purpose")[2];
+      var qs = decodeURIComponent(document.location.search);
+      if (qs) {
+        baseUri = qs.split("?ref=")[1];
+      }
 
       // Update the link urls
-      // for (let i = 0; i < links.length; ++i) {
+      // for (var i = 0; i < links.length; ++i) {
       //   links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
       //   links[i].textContent = links[i].href
       // }
 
-      let redirectInterval = setInterval(function () {
-        let countdown = document.querySelector(".countdown");
-        let t = parseInt(countdown.innerText, 10);
+      var redirectInterval = setInterval(function () {
+        var countdown = document.querySelector(".countdown");
+        var t = parseInt(countdown.innerText, 10);
         t -= 1;
 
         countdown.innerText = t;
 
         if (t === 0) {
           clearInterval(redirectInterval);
-          window.location.href =
-            baseUri +
-            "purpose" +
-            encodeURIComponent(purpose);
-            "productId" +
-            encodeURIComponent(productId);
+          window.location.href = baseUri 
+            // +
+            // "message=" +
+            // encodeURIComponent("Redirected automatically by timer");
         }
       }, 1000);
     });
