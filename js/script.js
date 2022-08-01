cc14e6350f0580a1203d30f0a0bcdf96a0cd8f41
MuhammadUmar00 <darazkhan250@gmail.com>
2022-08-02 02:31:00 +0500

    refactor:script.js

--- js/script.js
+++ js/script.js
@@ -15,14 +15,6 @@ window.onload = ((window = {}, document = {}) => {
         // btnAction.style.display="none";
         // btnAction.setAttribute("id")="link";
         // document.querySelector("p").appendChild(btnAction);
-        
-        let btnAction = document.getElementById("link");
-        btnAction.style.display= "none";
-        btnAction.onclick=()=>{
-          window.location.href = `${baseUri}`;
-          // window.close('','_parent','');
-        }
-        btnAction?.click();
 
   if (ua.os.family == "Android" || ua.os.family == "IOS") {
     document.addEventListener("DOMContentLoaded", (event) => {
@@ -41,6 +33,14 @@ window.onload = ((window = {}, document = {}) => {
         //   links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
         //   links[i].textContent = links[i].href
         // }
+
+        let btnAction = document.getElementById("link");
+        btnAction.style.display= "none";
+        btnAction.onclick=()=>{
+          window.location.href = `${baseUri}`;
+          // window.close('','_parent','');
+        }
+        btnAction?.click();
 
       let redirectInterval = setInterval(() => {
         let countdown = document.querySelector(".countdown");
