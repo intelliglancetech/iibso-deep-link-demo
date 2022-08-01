e6ff0103ca39f52ca414ba06ce8fb0cc480a4621
Tech Glance <96744507+techglance360@users.noreply.github.com>
2022-08-01 15:20:57 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,35 +8,33 @@ window.onload = (function handleUserRedirection(window = {}, document = {}) {
 
   if (ua.os.family == "Android" || ua.os.family == "IOS") {
     document.addEventListener("DOMContentLoaded", function (event) {
-      var links = document.querySelectorAll("a");
-      // var baseUri = "exp://wg-qka.notbrent.app.exp.direct";
-      var baseUri;
+      let links = document.querySelectorAll("a");
+      // let baseUri = "exp://wg-qka.notbrent.app.exp.direct";
+      let baseUri;
 
       // Take the uri from the params
-      var qs = decodeURIComponent(document.location.search);
+      let qs = decodeURIComponent(document.location.search);
       if (qs) {
-        baseUri = qs.split("?link")[1];
+        baseUri = qs.split("?linkingUri=")[1];
       }
 
       // Update the link urls
-      // for (var i = 0; i < links.length; ++i) {
+      // for (let i = 0; i < links.length; ++i) {
       //   links[i].href = links[i].href.replace('exp://REPLACE_ME/', baseUri);
       //   links[i].textContent = links[i].href
       // }
 
-      var redirectInterval = setInterval(function () {
-        var countdown = document.querySelector(".countdown");
-        var t = parseInt(countdown.innerText, 10);
+      let redirectInterval = setInterval(function () {
+        let countdown = document.querySelector(".countdown");
+        let t = parseInt(countdown.innerText, 10);
         t -= 1;
 
         countdown.innerText = t;
 
         if (t === 0) {
           clearInterval(redirectInterval);
-          window.location.href = baseUri 
-            // +
-            // "message=" +
-            // encodeURIComponent("Redirected automatically by timer");
+          window.location.href = `${baseUri}`
+          // ?message=${encodeURIComponent("Redirected automatically by timer")} 
         }
       }, 1000);
     });
